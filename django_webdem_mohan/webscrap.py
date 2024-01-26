76de7fda79be16ae8ba84b1a234298e8409534d9
Chiliverym <mohanchilivery31@gmail.com>
2024-01-26 12:11:29 +0530

    WebScraping with Libraray

---
+++ django_webdem_mohan/webscrap.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+def extract_data_from_shami_innovation(url):
+    try:
+        # Set a user agent to mimic a web browser
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+
+        # Send a GET request to the specified URL with headers
+        response = requests.get(url, headers=headers)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the HTML content using BeautifulSoup
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            # Find all paragraphs (P tags) in the HTML content
+            paragraphs = soup.find('h3',class_='heading')
+
+            # Extract and print the text content of each paragraph
+            for paragraph in paragraphs:
+                print(paragraph.get_text())
+
+        else:
+            print(f"Error: Unable to fetch data. Status code: {response.status_code}")
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+# Example usage:
+shami_innovation_url = "https://shamiit.com/"
+extract_data_from_shami_innovation(shami_innovation_url)
