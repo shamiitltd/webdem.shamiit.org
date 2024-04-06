15ca222ca8eed910477fbabb8a18675b50f0e2c2
sidharth <sidharthchawla484@gmail.com>
2024-04-06 19:15:15 +0530

    again update

--- webdem/urls.py
+++ webdem/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.layer1_UI , name="layer1"),
+    path('layer1/',views.layer1_UI , name="layer1"),
     path('layer3/',views.layer3_UI , name="layer3"),
   
 ]
