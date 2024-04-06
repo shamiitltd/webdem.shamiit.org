9127f2ece2a8ebd68025b8fb3a36b1838b1dae60
sidharth <sidharthchawla484@gmail.com>
2024-04-06 18:57:23 +0530

    UI for layer1 and layer3 Done

--- webdem/urls.py
+++ webdem/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from webdem import views
+from myapp import views
 
 urlpatterns = [
-    path('',views.index , name="index"),
+    path('',views.layer1_UI , name="layer1"),
+    path('layer3/',views.layer3_UI , name="layer3"),
+  
 ]
