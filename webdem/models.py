753b526c436b1a1116f6fc39e1eef3a1f816b97e
anshika0912 <anshumishra0067@gmail.com>
2024-04-09 14:11:25 +0530

    PUT Method (Update) - Example-Student has DOB and User-id name created by, created date, updated by, updated date.

--- webdem/models.py
+++ webdem/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    dob = models.DateField()
+    class Meta:
+        db_table = 'Student'
--- webdem/urls.py
+++ webdem/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from webdem import views
-
+from .views import *
 urlpatterns = [
     path('',views.index , name="index"),
+     path('get-students',getStudent),
+    path('post-student',postStudent),
+    path('put-student',putStudent),
 ]
